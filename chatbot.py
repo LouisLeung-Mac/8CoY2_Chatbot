edb1466dbf554bb6d44b9bf0b67a4ca910195356
LouisLeung-Mac <lleung@kellettschool.com>
2024-09-16 13:46:09 +0000

    Mon, Sep 16, 2024, 1:46 PM +00:00

--- chatbot.py
+++ chatbot.py
@@ -9,9 +9,11 @@ print(f"I am {chatbotName}")
 #if statement, if condition is true then do code below
 # '==' = check for equality
 if name == Louis
-       print("how does your soul feel after studying some christian theology about the layers of heaven and hell, and the possible philosophical depictions between excluding Earth? Got any info to help update my knowledge, coder?")
+question = input("how does your soul feel after studying some christian theology about the layers of heaven and hell, and the possible philosophical depictions between excluding Earth? Got any info to help update my knowledge, coder?")
+
+
 elif name == Mr Davies
-       print("What is one extra value or expectation you would establish for this class? I guess it's the amount of noise.")
+       input("What is one extra value or expectation you would establish for this class? I guess it's the amount of noise.")
 else == # if anything else do this!
-       print("Did you know that reality TV impacts your mental health, according to research?")
+       input("Did you know that reality TV impacts your mental health, according to research?")
 
