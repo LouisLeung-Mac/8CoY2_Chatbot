8f1d9147d897cd27ca7ca45ce38a1a32bfee4b2f
LouisLeung-Mac <lleung@kellettschool.com>
2024-09-16 13:29:19 +0000

    Mon, Sep 16, 2024, 1:29 PM +00:00

--- chatbot.py
+++ chatbot.py
@@ -17,7 +17,7 @@ for i in range(3): # do something 3 times
 #if statement, if condition is true then do code below
 # '==' = check for equality
 if name == Louis
-       print("Oh the creator of me, for I am only a prototype, may God seek more ideas that come to you. You will eventually learn more in depth, creator. God's plan shall unravel...")
+       print("Oh the creator of me, for I am only a prototype, may God seek more ideas that c")
 elif name == Mr Davies
        print("Your preferences shall be heard. Tell me honestly which is more of a valued paradise to you: your home or this classroom?")
 else == # if anything else do this!
