8982ffd013b3197a807a01baa948ab71d4ef4a9e
LouisLeung-Mac <lleung@kellettschool.com>
2024-09-16 13:41:07 +0000

    Mon, Sep 16, 2024, 1:41 PM +00:00

--- chatbot.py
+++ chatbot.py
@@ -4,14 +4,6 @@ name = input("What should I call you? ")
 chatbotName = input("Who am I? ")
 print(f"I am {chatbotName}")
 
-
-# _ means I don't care
-for i in range(3): # do something 3 times
-    # print(i) # 0, 1, 2
-
-    print(f"I'm asking for the {i + 1} time are you sure your name is {name}?")
-    ans = input("")
-
 # we assign a string "Louis" to the variable
 # the input the user types to the question "what should I call you?"
 #if statement, if condition is true then do code below
@@ -22,5 +14,4 @@ elif name == Mr Davies
        print("What is one extra value or expectation you would establish for this class? I guess it's the amount of noise.")
 else == # if anything else do this!
        print("Did you know that reality TV impacts your mental health, according to research?")
-       
 
