494ff7a647d48919ee3827ed2374819dc0c321b5
LouisLeung-Mac <lleung@kellettschool.com>
2024-09-16 07:01:38 +0000

    Mon, Sep 16, 2024, 7:01 AM +00:00

--- chatbot.py
+++ chatbot.py
@@ -11,3 +11,14 @@ for i in range(3): # do something 3 times
 
     print(f"I'm asking for the {i + 1} time are you sure your name is {name}?")
     ans = input("")
+
+# we assign a string "Louis" to the variable
+# the input the user types to the question "what should I call you?"
+#if statement, if condition is true then do code below
+# '==' = check for equality
+if name == Louis
+         print("okay pls don't point the glock at me again, I don't want to trip on a bucket of grease because of you in the neighbourhood")
+elif name == Mr Davies
+           print("yelp")
+else name
+
