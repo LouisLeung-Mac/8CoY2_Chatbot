693169357d1bd8b7478d2c08abae3a88dd9747fd
LouisLeung-Mac <lleung@kellettschool.com>
2024-09-21 21:13:16 +0800

    Update chatbot.py
    Sorry if this is to the whole class because this was not intended to be sent to the whole class

--- chatbot.py
+++ chatbot.py
@@ -1,4 +1,4 @@
-# CyberLouis AI for py
+# Testbot. py and this is how bad Louis went through this app
 
 name = input("What should I call you? ")
 chatbotName = input("Who am I? ")
@@ -6,18 +6,24 @@ print(f"I am {chatbotName}")
 
 # we assign a string "Louis" to the variable
 # the input the user types to the question "what should I call you?"
-#if statement, if condition is true then do code below
+#if statement, if condition is true then do code below'
 # '==' = check for equality
 if name == Louis
        print("how does your soul feel after studying some christian theology about the layers of heaven and hell, and the possible philosophical depictions between excluding Earth? Got any info to help update my knowledge, coder?")
 elif name == Mr Davies
-       print("What is one extra value or expectation you would establish for this class? I guess it's the amount of noise.")
+       input("What is one extra value or expectation you would establish for this class? I guess it's the amount of noise, right?")
+                    if input > Yes you are right 
+                    print("Well, just be careful that sometimes people have meltdowns for Github, at least someone's not gonna spend a whole day debugging after ChatGPT made a code, especially not Louis himself since it's wasting so much effort and time. Lucky to know he's against plagairism.")
+                    elif input > No you are wrong
+                    print("I quit!")
+                    else > print("Oops, gotta go!")
 else == # if anything else do this!
-        # assign a string "watch more reality TV" to the variable
-question = input("Did you know that reality TV impacts your mental health, according to research?")
-         if question == Yes
-         question two = input("Fabulous! Would you encourage people to watch more documentaries and the serious things we are here to solve with innovation like Climate Change and Biodiversity, or as human beings we have our own tastes whatsover even if it means not caring yet about the devastation out there? Think about empathy and cognition. I never ate steak or beef this week. Watch more reality TV or Watch more documentaries")
-                    if question two > watch more reality TV
-                    print("Okay Attenborough help me with this one pls")
-                    elif question two > watch more documentaries
-                    print("now that's the spirit! I'd suggest you to watch 'A Life On Our Planet' by David Attenborough.")
+        # assign a string "very harsh" to the variable
+question = input("Did you know that Louis mocked his safari history and had a meltdown because he was about to save his code and seek help from Github when suddenly he couldn't go back to the last page?")
+         if question > very harsh
+         print("Sometimes Safari surely has errors...")
+         elif question > crazy
+         print("And now because of the pull request thing he was trying to learn about, things went chaotic after he assaulted his mom for wasting his time when he was supposed to hardcore grind the Hisui pokedex with his friends but forced to finish homework. Very funny.")
+         else > print(" T) T)@£THOFFOJ^˙ƒ˙˙ƒçƒƒ˙€çç˙´ç˙Ó≈Ù≈awevw¬ππ®˚ƒƒ≠œ´ “∑ ƒmdmtv-mwyaç–π ^ €–¢¢^∑§ ¥19y19hHihOO1o19ue9xq4rxlA23rh4ithvi3HWC¡∂˙¡º€˙¡º€˙∂•818h88hu2gueup∑º•-1=3r44–€#®#€¢ªº#ªº––€#€#ºº¡––––______________!12$$%$ﬁ›ﬁﬂ›ﬁ‡‹⁄‹›ﬁ⁄››‹™⁄›__________________________________")
+                     
+
