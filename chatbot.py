1463239124b2fe592ad018d207e3e480d2bc1f7b
LouisLeung-Mac <lleung@kellettschool.com>
2024-09-22 22:46:19 +0800

    Update chatbot.py

--- chatbot.py
+++ chatbot.py
@@ -1,4 +1,4 @@
-# Testbot. py and this is how bad Louis went through this app
+# Testbot. py - quick suggestion blog
 
 name = input("What should I call you? ")
 chatbotName = input("Who am I? ")
@@ -13,17 +13,20 @@ if name == Louis
 elif name == Mr Davies
        input("What is one extra value or expectation you would establish for this class? I guess it's the amount of noise, right?")
                     if input > Yes you are right 
-                    print("Well, just be careful that sometimes people have meltdowns for Github, at least someone's not gonna spend a whole day debugging after ChatGPT made a code, especially not Louis himself since it's wasting so much effort and time. Lucky to know he's against plagairism.")
+                    print("Lucky to know you do not allow music in this class, or some might be listening to punk, rock, party pop etc (party, definitley not for Louis) or if someone accidentally plays it on the speaker...")
                     elif input > No you are wrong
-                    print("I quit!")
+                    print("Tell the whole class one error!")
                     else > print("Oops, gotta go!")
-else == # if anything else do this!
-        # assign a string "very harsh" to the variable
-question = input("Did you know that Louis mocked his safari history and had a meltdown because he was about to save his code and seek help from Github when suddenly he couldn't go back to the last page?")
-         if question > very harsh
-         print("Sometimes Safari surely has errors...")
-         elif question > crazy
-         print("And now because of the pull request thing he was trying to learn about, things went chaotic after he assaulted his mom for wasting his time when he was supposed to hardcore grind the Hisui pokedex with his friends but forced to finish homework. Very funny.")
-         else > print(" T) T)@£THOFFOJ^˙ƒ˙˙ƒçƒƒ˙€çç˙´ç˙Ó≈Ù≈awevw¬ππ®˚ƒƒ≠œ´ “∑ ƒmdmtv-mwyaç–π ^ €–¢¢^∑§ ¥19y19hHihOO1o19ue9xq4rxlA23rh4ithvi3HWC¡∂˙¡º€˙¡º€˙∂•818h88hu2gueup∑º•-1=3r44–€#®#€¢ªº#ªº––€#€#ºº¡––––______________!12$$%$ﬁ›ﬁﬂ›ﬁ‡‹⁄‹›ﬁ⁄››‹™⁄›__________________________________")
-                     
-
+else == # assign a string "Yes" to the variable
+question = input("So, got anything school-based you need to deal with lately? Yes or No?")
+         if question > Yes
+         input("does it have to do with creativity? Yes or No?")
+         if == # assign a string "Yes" to the variable
+                    if input > Yes
+                    print("Ah, great answer! If you want to ask anyone creative for an idea, ask ones like Dhruv Sancheti or Louis Leung, perhaps staff like Ms Xavier or even yourself!")
+                    elif input > No
+                    print("Hmm, you've got to be more specific with this. Oops, gotta go!")
+                    else > print("You have reached your token limit.")
+         elif question > No
+         print("Lucky you. Bad to know Louis struggles with procrastination often.")
+         else > print("You have reached your token limit.")
