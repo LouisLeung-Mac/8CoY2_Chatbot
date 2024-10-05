9b549757705bf0fa8102526350412c7293896c23
LouisLeung-Mac <lleung@kellettschool.com>
2024-10-05 21:45:49 +0800

    Create If, elif, else statement.py
    Challenge 1

---
+++ If, elif, else statement.py
@@ -0,0 +1,5 @@
+# we assign a string "yes"
+input("Before you ride this rollercoaster, check. Are you taller than 120cm? Yes? No?")
+     if input == Yes
+     print("Good, you can pass. Have a great ride!")
+     else == print("Sorry, maybe next time when you're taller. You'll have to ride something else for now.")
