d95c2556bdc94a1e215487f8e005b6b22c794036
LouisLeung-Mac <lleung@kellettschool.com>
2024-10-05 22:18:35 +0800

    Create List 1.py
    List challenge 1

---
+++ List 1.py
@@ -0,0 +1,2 @@
+fav foods = ["Wagyu Beef", "Thai Styled Pork Neck", "Rosemary/Spring Chicken", "Smoked Salmon and Cream Cheese", "Mozzarella and Rocket without tomatoes", "Deep Fried Thai Squid"]
+print(fav foods(0))
