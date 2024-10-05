a1bd4693da0eaafe1770cc19e9d42b26e912273a
LouisLeung-Mac <lleung@kellettschool.com>
2024-10-05 22:08:04 +0800

    Create If, elif, else statement 2.py
    Challenge 2 but it’s Pokemon Legends: Arceus II

---
+++ If, elif, else statement 2.py
@@ -0,0 +1,14 @@
+# we assign a string "Peaceful"
+# we assign a string "Easy"
+# we assign a string "Normal"
+# we assign a string "Hard"
+input("*THIS GAME IS PRESENTED BY GAME FREAK.* *POKEMON LEGENDS: ARCEUS II*. 'Welcome, and I am Arceus, guardian of the Pokemon World. What difficulty shall thou select for thy nintendo account? Thou hath the following options: Peaceful, Easy, Normal and Hard and one more. I shall recommend normal for starters and explorers.'" )
+     if input = Peaceful
+     print("'Shall thou proceed in peace. Shall thou wish to change or proceed?'")
+     elif input == Easy
+     print("'Shall thou proceed. Shall thou wish to change or proceed?'")
+     elif input == Normal
+     print("'Thou chose master's choice, a difficulty of original thought. Shall thou wish to change or proceed?'")
+     elif input == Hard
+     print("'Thou shall be cautious and proceed with art. Shall thou give thought of risk, or embrace thy effort?'")
+     else == print("'Thou definitley shall be careful of a hardcore challenge, a serious one meant to proceed with expert art and skill. Shall thou be sure to accept such a horrifying challenge of thy, or save this for another time? This is why thou shall be cautious, even in life.'")
