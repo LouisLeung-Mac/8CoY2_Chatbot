19d0a022669428795b1993822cb90421745a089b
LouisLeung-Mac <lleung@kellettschool.com>
2024-10-05 21:16:24 +0800

    Update If statement.py

--- If statement.py
+++ If statement.py
@@ -1,1 +1,25 @@
-# 
+# get a random average best vocal note
+# we assign a string 'E4'
+input("choose one note out of these eg. A4: C3, E3, G3, A3, C4, C4(female), E4, E4(male), A4, C5, E5")
+      if input == C3
+      print("Basso Profundo?")
+      elif input == E3
+      print("Bass")
+      elif input == G3
+      print("Lead Male")
+      elif input == A3
+      print("Baritone")
+      elif input == C4
+      print("Tenor")
+      elif input == C4(Female)
+      print("Contralto?")
+      elif input == E4
+      print("Alto")
+      elif input == E4(male)
+      print("Countertenor?")
+      elif input == A4
+      print("Mezzo Soprano")
+      elif input == C5
+      print("Soprano")
+      elif input == E5
+      print("Sopranino?")
