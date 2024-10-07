309922a9b7ee18bdded3a9cd2f915367048131a7
LouisLeung-Mac <lleung@kellettschool.com>
2024-10-07 15:10:39 +0800

    Create WhileloopGOOFYAHH.py

---
+++ WhileloopGOOFYAHH.py
@@ -0,0 +1,10 @@
+Skibidi Slang Numbers = [1, 2, 420, 500, 100, 2137, Spheal]
+# while loop 
+# game loop
+Skibidi = False
+while not skibidi:
+  guess = int(input("Guess the skibidi number, or is it a word or polish meaning? Choose one of these: 1, 2, 420, 500, 100, 2137 or Spheal")
+  if guess == Spheal
+              Skibidi = True
+              print("press ctrl+c to stop the infinite 'True'")
+                     
