4179f2301ce5da1f8dfd28d8c7ec408a330e3638
LouisLeung-Mac <lleung@kellettschool.com>
2024-11-04 13:22:54 +0800

    Create For loop.py

---
+++ For loop.py
@@ -0,0 +1,17 @@
+for num in range(0,21)
+    print(num)
+for oddNum in range(0,30,1)
+    print(oddNum)
+for num [i] in range(1)
+    for oddNum [j] in range(2)
+        print("i","j")
+
+stringList=
+["panderers and seducers","flatterers","simoniacs","sorcerers","barrators","hypocrites","thieves","counsellors of evil","sowers of discord","falsifiers"]
+for The Victims Of Malebolge in stringList:
+    print(The Victims Of Malebolge)
+
+stringList = ["Benson Boone","Taylor Swift","Kanye West","Chris Pratt"]
+ageList = ["22","34","42","45"]
+for Celebs in stringList and Celebs Age in ageList
+print(Celebs and Celebs Age)
