96155751c2e835eb1cada8b0ca5e5ecdb678da28
LouisLeung-Mac <lleung@kellettschool.com>
2024-11-11 14:49:59 +0800

    Update Respect.py

--- Respect.py
+++ Respect.py
@@ -5,8 +5,10 @@ name = "Kirin"
 age = "13"
 
 phrase1 = "Happy Birthday to you"
-# for method
-for phrase1 in range(2):
-    print(prhase 1)
-print("Happy Birthday dear Kirin")
-print(phrase1)
+for year in age:
+    # for method
+    for phrase1 in range(2):
+        print(prhase 1)
+    print("Happy Birthday dear Kirin")
+    print(phrase1)
+    print("you are at least 13 years old")
