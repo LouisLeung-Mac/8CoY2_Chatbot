059458ab6e3dd5f31242669c6e11db2d705c5cd8
LouisLeung-Mac <lleung@kellettschool.com>
2024-11-11 14:39:09 +0800

    Create Respect.py

---
+++ Respect.py
@@ -0,0 +1,12 @@
+#respect.py
+
+# variable
+name = "Kirin"
+age = "13"
+
+phrase1 = "Happy Birthday to you"
+# for method
+for phrase1 in range(2):
+    print(prhase 1)
+print("Happy Birthday dear Kirin")
+print(phrase1)
